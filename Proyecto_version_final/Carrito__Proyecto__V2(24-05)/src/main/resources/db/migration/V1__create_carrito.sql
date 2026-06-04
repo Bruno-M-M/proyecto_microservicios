39e181c4cb0a049902219efac04a6c26257603cd
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-04 17:57:57 -0400

    Delete Proyecto_version_final directory

--- Proyecto_version_final/Carrito__Proyecto__V2(24-05)/src/main/resources/db/migration/V1__create_carrito.sql
+++
@@ -1,8 +0,0 @@
-CREATE TABLE IF NOT EXISTS carrito (
-                                       id               BIGINT AUTO_INCREMENT PRIMARY KEY,
-                                       cliente_id       BIGINT      NOT NULL,
-                                       fecha_creacion   DATETIME    NOT NULL DEFAULT CURRENT_TIMESTAMP,
-                                       fechaConfirmacion DATETIME   NULL,
-                                       estado           VARCHAR(20) NOT NULL DEFAULT 'PENDIENTE',
-    CONSTRAINT chk_estado CHECK (estado IN ('PENDIENTE','PAGADO','CANCELADO'))
-);

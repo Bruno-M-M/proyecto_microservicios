f6c52a12081e40f0c5a6dbac7bf7cd7c7f5596af
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-16 17:48:02 -0400

    Se implemento Swagger
    Se implemento swagger y funciona

---
+++ Carrito__Proyecto/src/main/java/com/Carrito/Carrito_compras/Config/SwaggerConfig.java
@@ -0,0 +1,20 @@
+package com.Carrito.Carrito_compras.Config;
+
+import io.swagger.v3.oas.models.OpenAPI;
+import io.swagger.v3.oas.models.info.Info;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class SwaggerConfig {
+
+    @Bean
+    public OpenAPI customOpenApi(){
+        return new OpenAPI()
+                .info(new Info()
+                        .title("API 2026 Clientes")
+                        .version("1.4")
+                        .description("Documentacion de la API para el sistema de almacenacionto de datos del CLiente")
+                );
+    }
+}

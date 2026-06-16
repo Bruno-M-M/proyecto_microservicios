b29d75c1bd1bc41e6e3b041789a8eab2f292422a
Benjamin Martinez <benja.martinezl@duocuc.cl>
2026-06-16 18:21:45 -0400

    Add files via upload

--- Pago_Proyecto/src/main/java/com/Pago/Metodo_de_pago/Controller/BoletaController.java
+++ Pago_Proyecto/src/main/java/com/Pago/Metodo_de_pago/Controller/BoletaController.java
@@ -3,6 +3,10 @@ package com.Pago.Metodo_de_pago.Controller;
 import com.Pago.Metodo_de_pago.DTO.BoletaRequestDTO;
 import com.Pago.Metodo_de_pago.DTO.BoletaResponseDTO;
 import com.Pago.Metodo_de_pago.Service.BoletaService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.responses.ApiResponse;
+import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -10,11 +14,26 @@ import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
+
 @RestController
 @RequestMapping("/api/v1/boletas")
 public class BoletaController {
     @Autowired
     private BoletaService boletaService;
+
+
+    @Operation(
+            summary = "Emitir una boleta",
+            description = "Genera una nueva boleta de pago a partir de los datos del pedido. Calcula el IVA automáticamente"
+    )
+    @ApiResponses(
+            {@ApiResponse(
+                    responseCode = "201",
+                    description = "Boleta emitida con exito",
+                    content = @Content(mediaType = "application ")
+
+            )}
+    )
     @PostMapping
     public ResponseEntity<BoletaResponseDTO> emitir(@Valid @RequestBody BoletaRequestDTO request) {
         return ResponseEntity.status(201).body(boletaService.emitirBoleta(request));

e232edcfbb3e092eb45b17f7354a6cf5eac064da
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-18 18:49:25 -0400

    fix
    Implementacion parcial de hateos

--- Cliente_Proyecto/src/main/java/proyecto/cliente/ClienteApplication.java
+++ Cliente_Proyecto/src/main/java/proyecto/cliente/ClienteApplication.java
@@ -1,10 +1,11 @@
 package proyecto.cliente;
 
+import org.springdoc.core.configuration.SpringDocHateoasConfiguration;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.openfeign.EnableFeignClients;
 
-@SpringBootApplication
+@SpringBootApplication(exclude = {SpringDocHateoasConfiguration.class})
 @EnableFeignClients
 public class ClienteApplication {
 

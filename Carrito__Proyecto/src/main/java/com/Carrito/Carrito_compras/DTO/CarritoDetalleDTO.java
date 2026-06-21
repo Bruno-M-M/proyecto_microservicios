06b4cd8d02e36ebd56ab8c0adee1828b8cda0d61
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-20 21:44:32 -0400

    Add files via upload

---
+++ Carrito__Proyecto/src/main/java/com/Carrito/Carrito_compras/DTO/CarritoDetalleDTO.java
@@ -0,0 +1,28 @@
+package com.Carrito.Carrito_compras.DTO;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDateTime;
+import java.util.List;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Data
+public class CarritoDetalleDTO {
+    private Long pedidoId;
+    private String estadoPedido;
+    private LocalDateTime fechaCreacion;
+    private LocalDateTime fechaConfirmacion;
+    private Double totalPedido;
+
+    private Long clienteId;
+    private String clienteRun;
+    private String clienteNombre;
+    private String clienteCorreo;
+    private String clienteDireccion;
+    private Integer clienteTelefono;
+
+    private List<CarritoItemDetalleDTO> items;
+}

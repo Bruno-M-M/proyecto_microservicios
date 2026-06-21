e693a1293b20cea7fa264d1f101774ac1816ccc1
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-20 21:54:06 -0400

    rename

---
+++ ClienteProyecto/src/main/java/proyecto/cliente/ClienteApplication.java
@@ -0,0 +1,16 @@
+package proyecto.cliente;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
+
+@SpringBootApplication()
+@EnableFeignClients
+public class ClienteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ClienteApplication.class, args);
+	}
+
+}

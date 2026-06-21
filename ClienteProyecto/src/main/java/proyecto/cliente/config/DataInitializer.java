d9cf22962f6e7d6688fd42cbb50c1348c05a3889
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-21 18:15:23 -0400

    Add files via upload

--- ClienteProyecto/src/main/java/proyecto/cliente/config/DataInitializer.java
+++ ClienteProyecto/src/main/java/proyecto/cliente/config/DataInitializer.java
@@ -24,7 +24,7 @@ public class DataInitializer implements CommandLineRunner {
         }
 
         log.info(">>> Cargando clientes iniciales...");
-        repository.save(new Cliente(null, 206888807, "5", "Bruno Mateluna", "br.mateluna@duocuc.cl", "Calle quinta vergara 666", 949000000, passwordEncoder.encode("abc123.")));
+        repository.save(new Cliente(null, 206854321, "5", "Bruno Mateluna", "br.maasda@dasda.cl", "Calle quinta 666", 949000000, passwordEncoder.encode("abc123.")));
         repository.save(new Cliente(null, 12345678, "9", "Claudio Bravo", "cl.bravo@chile.cl", "Av.santiago 522", 967342652, passwordEncoder.encode("chile1234")));
         log.info(">>> 2 Clientes cargados OK.");
     }

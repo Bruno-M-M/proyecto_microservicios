434de44141de470bd0bb77213919c410c5ba35db
Bruno-M-M <br.mateluna@duocuc.cl>
2026-06-21 22:59:00 -0400

    Add files via upload

---
+++ Inventario_Proyecto/src/main/java/proyecto/inventario/controller/HomeController.java
@@ -0,0 +1,14 @@
+package proyecto.inventario.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HomeController {
+
+    @GetMapping("/actuator/info")
+    public String info() {
+        return "redirect:/doc/swagger-ui.html";
+    }
+}
+
